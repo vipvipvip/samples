2d0298a76bf10271eb447a46aba09ccd2901a9c3
dikesh.chokshi@universalorlando.com <dikesh.chokshi@universalorlando.com>
2017-05-30 15:57:15 -0400

    More modularizing

---
+++ lodash/services.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('CityService')
+        .service('cityservice', Service);
+
+    Service.$inject = [$http, $q];
+    function Service($http, $q) {
+        this.getCities = getCities;
+        
+        ////////////////
+        function getCities() {  
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/lodash/cities.json',
+                cache: true
+            }).then(function(response) {
+                d.resolve(response.data);
+            },function(err) {
+                d.reject(err);
+            });
+            return d.promise;
+        }
+    }
+})();
