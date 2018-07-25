0e2163f3f79a62422045a84c100404263d45e622
UNIV-CITY-FLA\206583534 <Dikesh1.Chokshi@universalorlando.com>
2018-07-25 09:56:00 -0400

    + a variation for modules

--- bits2/app.js
+++ bits2/app.js
@@ -1,8 +1,23 @@
-(function() {
-    'use strict';
+// (function() {
+//     'use strict';
 
-    angular.module('bits', [
+//     angular.module('bits', [
         
-    ])
+//     ])
     
-})();
+// })();
+
+
+import * as ng from 'angular';
+
+import 'angular-ui-router';
+import 'angular-toastr';
+import 'angular-messages';
+
+const externalDependencies = [
+  'ui.router',
+  'toastr',
+  'ngMessages'
+];
+
+export default ng.module('bits', externalDependencies);
